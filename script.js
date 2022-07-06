038600833cc0a91ac86b2dd8610b6f570a199050
Josh <jlee12297@gmail.com>
2022-07-05 21:44:50 -0700

    have gotten one functioning row for saving into local storage and displaying. need to add function for all rows

--- script.js
+++ script.js
@@ -18,7 +18,7 @@ for (i=0; i<rowId.length; i++) {
     if (currentHour === i+9 && currentHour < 16) {
         rowId[i].setAttribute("style", "background-color:red;");
         rowDescriptions[i].setAttribute("style", "background-color:red;");
-    } else if (currentHour > i+9 && currentHour < 16) {
+    } else if (currentHour < i+9 && currentHour < 16) {
         rowId[i].setAttribute("style", "background-color:green;");
         rowDescriptions[i].setAttribute("style", "background-color:green;");
     } else {
@@ -26,3 +26,128 @@ for (i=0; i<rowId.length; i++) {
         rowDescriptions[i].setAttribute("style", "background-color:gray;");
 
 }}
+
+// variables for the user inputs of the description column for each hour
+var description9 = $('input[id="desc9"]').val();
+var description10 = $('input[id="desc10"]').val();
+var description11 = $('input[id="desc11"]').val();
+var description12 = $('input[id="desc12"]').val();
+var description13 = $('input[id="desc13"]').val();
+var description14 = $('input[id="desc14"]').val();
+var description15 = $('input[id="desc15"]').val();
+var description16 = $('input[id="desc16"]').val();
+var description17 = $('input[id="desc17"]').val();
+
+//runs whenever page loads to render stored descriptions when applicable, for each hour
+function renderDesc9() {
+    var storedDescription9 = JSON.parse(localStorage.getItem("description9"));
+    if (storedDescription9 !== null) {
+        $('input[id="desc9"]').val(storedDescription9)
+        console.log(storedDescription9);
+    } else {
+        console.log("cannot find")
+        return;
+    }
+}
+
+
+// save button event listener functionality. one for each button w/ its corresponding input for description will be made.
+//9AM row
+var button9 = $('#btn9');
+
+button9.on('click', function () {
+    description9 = $('input[id="desc9"]').val();
+    console.log(description9);
+    localStorage.setItem("description9", JSON.stringify(description9));
+
+    })
+
+
+//10AM row
+var button10 = $('#btn10');
+
+button10.on('click', function () {
+    var description10 = $('input[id="desc10"]').val();
+    console.log(description10);
+    
+    })
+
+
+//11AM row
+var button11 = $('#btn11');
+
+button11.on('click', function () {
+    var description11 = $('input[id="desc11"]').val();
+    console.log(description11);
+    
+    })
+
+
+//12PM row
+var button12 = $('#btn12');
+
+button12.on('click', function () {
+    var description12 = $('input[id="desc12"]').val();
+    console.log(description12);
+        
+    })
+
+
+//1PM row
+var button13 = $('#btn13');
+
+button13.on('click', function () {
+    var description13 = $('input[id="desc13"]').val();
+    console.log(description13);
+    
+    })
+
+
+//2PM row
+var button14 = $('#btn14');
+
+button14.on('click', function () {
+    var description14 = $('input[id="desc14"]').val();
+    console.log(description14);
+    
+    })
+
+
+//3PM row
+var button15 = $('#btn15');
+
+button15.on('click', function () {
+    var description15 = $('input[id="desc15"]').val();
+    console.log(description15);
+    
+    })
+
+
+//4PM row
+var button16 = $('#btn16');
+
+button16.on('click', function () {
+    var description16 = $('input[id="desc16"]').val();
+    console.log(description16);
+    
+    })
+
+
+//5PM row
+var button17 = $('#btn17');
+
+button17.on('click', function () {
+    var description17 = $('input[id="desc17"]').val();
+    console.log(description17);
+    
+    })
+
+function init() {
+    renderDesc9();
+}
+
+
+init();
+
+
+    
